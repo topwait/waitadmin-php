bc4470a0fe52a95c4fb272e78ba6f22069846067
windy <10652944+wafts@user.noreply.gitee.com>
2022-12-22 16:44:33 +0800

    init

--- server/addons/curd/controller/GenController.php
+++ server/addons/curd/controller/GenController.php
@@ -1,5 +1,16 @@
 <?php
-
+// +----------------------------------------------------------------------
+// | WaitAdmin快速开发后台管理系统
+// +----------------------------------------------------------------------
+// | 欢迎阅读学习程序代码,建议反馈是我们前进的动力
+// | 程序完全开源可支持商用,允许去除界面版权信息
+// | gitee:   https://gitee.com/wafts/WaitAdmin
+// | github:  https://github.com/topwait/waitadmin
+// | 官方网站: https://www.waitadmin.cn
+// | WaitAdmin团队版权所有并拥有最终解释权
+// +----------------------------------------------------------------------
+// | Author: WaitAdmin Team <2474369941@qq.com>
+// +----------------------------------------------------------------------
 
 namespace addons\curd\controller;
 
@@ -22,6 +33,9 @@ use think\response\File;
 use think\response\Json;
 use think\response\View;
 
+/**
+ * 代码生成器
+ */
 class GenController extends Backend
 {
     /**
@@ -199,5 +213,4 @@ class GenController extends Backend
         \think\facade\View::engine('Think')->config($viewConfig);
         return view('', ['keys'=>$keys, 'values'=>$values]);
     }
-
 }
