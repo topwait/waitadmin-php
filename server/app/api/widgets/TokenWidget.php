e1392fadcc8b743fdc31334b2ab8865b98e429d4
windy <10652944+wafts@user.noreply.gitee.com>
2023-02-06 23:34:01 +0800

    init

--- server/app/api/widgets/TokenWidget.php
+++ server/app/api/widgets/TokenWidget.php
@@ -2,6 +2,8 @@
 
 namespace app\api\widgets;
 
+use think\facade\Cache;
+
 class TokenWidget
 {
     /**
@@ -9,6 +11,35 @@ class TokenWidget
      */
     public static function login()
     {
+        $cache = Cache::get('login:session:1');
+        if (!$cache) {
+            $build = [
+                'id' => 'admin:login:session:1',
+                'createTime' => time(),
+                'updateTime' => time(),
+                'tokenLists' => [
+                    [
+                        'value'         => 'OwCAnCJbE43siVV4TliYJy',
+                        'device'        => 1,
+                        'lastOpTime'    => time(),
+                        'lastIpAddress' => request()->ip(),
+                        'lastUaBrowser' => request()->header('User-Agent')
+                    ]
+                ]
+            ];
+        } else {
+            $cacheArray = json_decode($cache, true);
+            $cacheArray['updateTime'] = time();
+            $tokenLists = [];
+            foreach ($cacheArray['tokenLists'] as $item) {
+                if ($item['value'] = '') {
+                    $item['lastOpTime'] = time();
+                    $item['lastIpAddress'] = request()->ip();
+                    $item['lastUaBrowser'] = request()->header('User-Agent');
+                }
+            }
+        }
+
         $session = [
             'id' => 'admin:login:session:1',
             'createTime' => time(),
