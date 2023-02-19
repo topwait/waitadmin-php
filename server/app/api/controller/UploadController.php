74adada15ebe305b9d208108cb846b64861248a9
windy <10652944+wafts@user.noreply.gitee.com>
2023-02-19 21:41:00 +0800

    init

--- server/app/api/controller/UploadController.php
+++ server/app/api/controller/UploadController.php
@@ -2,9 +2,34 @@
 
 namespace app\api\controller;
 
+use app\api\service\UploadService;
 use app\common\basics\Api;
+use app\common\utils\AjaxUtils;
+use think\response\Json;
 
 class UploadController extends Api
 {
+    /**
+     * 上传图片
+     *
+     * @return Json
+     * @author windy
+     */
+    public function image(): Json
+    {
+        $result = UploadService::image();
+        return AjaxUtils::success($result);
+    }
 
+    /**
+     * 上传视频
+     *
+     * @return Json
+     * @author windy
+     */
+    public function video(): Json
+    {
+        $result = UploadService::image();
+        return AjaxUtils::success($result);
+    }
 }
---
+++ server/app/api/service/UploadService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\api\service;
+
+use app\common\basics\Service;
+
+class UploadService extends Service
+{
+    /**
+     * 上传图片
+     *
+     * @return array
+     * @author windy
+     */
+    public static function image(): array
+    {
+        return [];
+    }
+
+    /**
+     * 上传视频
+     *
+     * @return array
+     * @author windy
+     */
+    public static function video(): array
+    {
+        return [];
+    }
+}
