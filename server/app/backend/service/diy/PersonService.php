26e28a0aea7528b9076f6af1bee814d8134c412b
windy <10652944+wafts@user.noreply.gitee.com>
2023-03-10 18:02:10 +0800

    init

--- server/app/backend/service/diy/PersonService.php
+++ server/app/backend/service/diy/PersonService.php
@@ -18,6 +18,7 @@ namespace app\backend\service\diy;
 use app\common\basics\Service;
 use app\common\utils\ConfigUtils;
 use app\common\utils\UrlUtils;
+use JetBrains\PhpStorm\NoReturn;
 
 /**
  * 个人中心装修服务类
@@ -33,15 +34,10 @@ class PersonService extends Service
     public static function detail(): array
     {
         $detail = ConfigUtils::get('diy', 'person');
-        if (!$detail) {
-            return  [
-                'base' => ['layout'=>'row', 'title'=>''],
-                'list' => []
-            ];
-        }
-
-        foreach ($detail['list'] as &$item) {
-            $item['image'] = UrlUtils::toAbsoluteUrl($item['image']??'');
+        foreach ($detail as &$item) {
+            foreach ($item['list'] as &$val) {
+                $val['image'] = UrlUtils::toAbsoluteUrl($val['image']);
+            }
         }
 
         return $detail;
@@ -50,22 +46,33 @@ class PersonService extends Service
     /**
      * 装修数据保存
      *
-     * @param array $post
+     * @param array $params
      * @author windy
      */
-    public static function save(array $post): void
+    public static function save(array $params): void
     {
-        $list = $post['list']??[];
-        foreach ($list as &$item) {
-            $item['image'] = UrlUtils::toAbsoluteUrl($item['image']);
+        $data = [];
+        foreach ($params as $type => $item) {
+            $data[$type]['base'] = match ($type) {
+                'service' => [
+                    'layout' => $item['base']['layout'] ?? 'row',
+                    'title'  => $item['base']['title'] ?? ''
+                ],
+                'adv' => [
+                    'open' => intval($item['base']['open'] ?? '0')
+                ],
+            };
+
+            $data[$type]['list'] = [];
+            foreach ($item['list']??[] as $value) {
+                $data[$type]['list'][] = [
+                    'image' => UrlUtils::toRelativeUrl($value['image']??''),
+                    'name'  => $value['name']??'',
+                    'link'  => $value['link']??'',
+                ];
+            }
         }
 
-        ConfigUtils::set('diy', 'person', [
-            'base' => [
-                'layout' => $post['base']['layout']??'row',
-                'title'  => $post['base']['title']??''
-            ],
-            'list' => $list
-        ]);
+        ConfigUtils::set('diy', 'person', $data);
     }
 }
