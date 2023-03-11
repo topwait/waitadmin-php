3b4a57c9b52a15627a3ab11a07ec2e7f0456ac71
windy <10652944+wafts@user.noreply.gitee.com>
2023-03-11 15:07:04 +0800

    init

--- server/app/api/controller/DiyController.php
+++ server/app/api/controller/DiyController.php
@@ -1,20 +1,42 @@
 <?php
+// +----------------------------------------------------------------------
+// | WaitAdmin快速开发后台管理系统
+// +----------------------------------------------------------------------
+// | 欢迎阅读学习程序代码,建议反馈是我们前进的动力
+// | 程序完全开源可支持商用,允许去除界面版权信息
+// | gitee:   https://gitee.com/wafts/WaitAdmin
+// | github:  https://github.com/topwait/waitadmin
+// | 官方网站: https://www.waitadmin.cn
+// | WaitAdmin团队版权所有并拥有最终解释权
+// +----------------------------------------------------------------------
+// | Author: WaitAdmin Team <2474369941@qq.com>
+// +----------------------------------------------------------------------
+declare (strict_types = 1);
 
 namespace app\api\controller;
 
-
 use app\api\service\DiyService;
-use app\api\service\UserService;
+use app\common\basics\Api;
 use app\common\utils\AjaxUtils;
 use think\response\Json;
 
-class DiyController
+/**
+ * 装修管理
+ */
+class DiyController extends Api
 {
     protected array $notNeedLogin = ['index', 'tie', 'me'];
 
-    public function index()
+    /**
+     * 首页页面装修
+     *
+     * @return Json
+     * @author windy
+     */
+    public function index(): Json
     {
-
+        $result = DiyService::index();
+        return AjaxUtils::success($result);
     }
 
     /**
--- server/app/api/service/DiyService.php
+++ server/app/api/service/DiyService.php
@@ -1,9 +1,21 @@
 <?php
+// +----------------------------------------------------------------------
+// | WaitAdmin快速开发后台管理系统
+// +----------------------------------------------------------------------
+// | 欢迎阅读学习程序代码,建议反馈是我们前进的动力
+// | 程序完全开源可支持商用,允许去除界面版权信息
+// | gitee:   https://gitee.com/wafts/WaitAdmin
+// | github:  https://github.com/topwait/waitadmin
+// | 官方网站: https://www.waitadmin.cn
+// | WaitAdmin团队版权所有并拥有最终解释权
+// +----------------------------------------------------------------------
+// | Author: WaitAdmin Team <2474369941@qq.com>
+// +----------------------------------------------------------------------
+declare (strict_types = 1);
 
 namespace app\api\service;
 
 use app\common\basics\Service;
-use app\common\utils\AjaxUtils;
 use app\common\utils\ConfigUtils;
 use app\common\utils\UrlUtils;
 use JetBrains\PhpStorm\ArrayShape;
@@ -13,9 +25,15 @@ use JetBrains\PhpStorm\ArrayShape;
  */
 class DiyService extends Service
 {
-    public function index()
+    /**
+     * 首页页面装修
+     *
+     * @return array
+     * @author windy
+     */
+    public static function index(): array
     {
-
+        return [];
     }
 
     /**
