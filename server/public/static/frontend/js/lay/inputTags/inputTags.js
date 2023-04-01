2472ac11a5b7aa95d7adea73b4fe32b09878e24d
windy <10652944+wafts@user.noreply.gitee.com>
2023-04-01 17:07:51 +0800

    优化标签生成器

--- server/public/static/frontend/js/lay/inputTags/inputTags.js
+++ server/public/static/frontend/js/lay/inputTags/inputTags.js
@@ -1,69 +1,77 @@
-/*
-* @Author: layui-2
-* @Date:   2018-08-31 11:40:42
-* @Last Modified by:   hi916
-* @Last Modified time: 2021-04-15 15:52:33
-* @Last Modified description:添加了flatContent,用于openFlatContent的对象去重;添加了pinArray用于控制标签是否可删除
-*/
+// +----------------------------------------------------------------------
+// | 标签生成器
+// +----------------------------------------------------------------------
+
 layui.define(['jquery', 'layer'], function (exports) {
-    "use strict";
+    'use strict';
     let $ = layui.jquery;
     let MOD_NAME = 'inputTags';
 
-    // 外部接口
+    /**
+     * 外部接口
+     */
     let inputTags = {
         config: {flatContent: []}
+        ,on: function (events, callback) {
+            return layui.onevent.call(this, MOD_NAME, events, callback)
+        }
         ,set: function (options) {
             let that = this;
             that.config = $.extend({}, that.config, options);
             return that;
         }
-        ,on: function (events, callback) {
-            return layui.onevent.call(this, MOD_NAME, events, callback)
-        }
     };
 
-    // 当前实例
+    /**
+     * 当前实例
+     */
     let thisInputTags = function () {
         let that = this;
-        let options = that.config;
         return {
-            config: options
+            config: that.config
         }
     };
 
-    // 补全标签
+    /**
+     * 补全标签
+     */
     let fillTags = function (val, spans, options, that) {
         if (options.openFlatContent) {
             options.flatContent.push(val);
         }
         options.content.push(val);
         that.render();
-        if (options.pinArray.indexOf(val) === -1) {
-            spans = '<span><em>' + val + '</em><button type="button" class="close">×</button></span>';
-        } else {
+        if (options.pinArray.indexOf(val) !== -1) {
             spans = '<span><em>' + val + '</em></span>';
+        } else {
+            spans = '<span><em>' + val + '</em><button type="button" class="close">×</button></span>';
         }
         options.elem.before(spans)
     };
 
-    // 取构造器
+    /**
+     * 取构造器
+     */
     let Class = function (options) {
         let that = this;
         that.config = $.extend({}, that.config, inputTags.config, options);
         that.render();
     };
 
-    // 默认配置
+    /**
+     * 默认配置
+     */
     Class.prototype.config = {
         close: false            // 关闭按钮
+        ,openFlatContent: false // 是否开启
         ,theme: ''              // 背景颜色
         ,content: []            // 默认标签
         ,pinArray: []           // 需要取消删除按钮的元素
-        ,openFlatContent: false // 是否开启flatContent
     };
 
-    // 初始对象
+    /**
+     * 初始对象
+     */
     Class.prototype.init = function () {
         let that = this;
         let spans = '';
@@ -73,10 +81,10 @@ layui.define(['jquery', 'layer'], function (exports) {
             if (options.openFlatContent && options.flatContent.indexOf(item) === -1) {
                 options.flatContent.push(item)
             }
-            if (options.pinArray.indexOf(item) === -1) {
-                spans += '<span><em>' + item + '</em><button type="button" class="close">×</button></span>';
-            } else {
+            if (options.pinArray.indexOf(item) !== -1) {
                 spans += '<span><em>' + item + '</em></span>';
+            } else {
+                spans += '<span><em>' + item + '</em><button type="button" class="close">×</button></span>';
             }
         });
 
@@ -84,7 +92,9 @@ layui.define(['jquery', 'layer'], function (exports) {
         that.events()
     };
 
-    // 渲染对象
+    /**
+     * 渲染对象
+     */
     Class.prototype.render = function () {
         let that = this;
         let options = that.config;
@@ -92,7 +102,9 @@ layui.define(['jquery', 'layer'], function (exports) {
         that.enter()
     };
 
-    // 生成标签
+    /**
+     * 生成标签
+     */
     Class.prototype.enter = function () {
         let that  = this;
         let spans = '';
@@ -104,7 +116,7 @@ layui.define(['jquery', 'layer'], function (exports) {
                 let $val = options.elem.val().trim();
                 if (!$val) return false;
                 if (options.openFlatContent) {
-                    if (options.content.indexOf($val) === -1) {
+                    if (-1 === options.content.indexOf($val)) {
                         if (options.flatContent.indexOf($val) === -1) {
                             fillTags($val, spans, options, that)
                         }
@@ -114,10 +126,10 @@ layui.define(['jquery', 'layer'], function (exports) {
                         fillTags($val, spans, options, that)
                     }
                 }
-                options.done && typeof options.done === 'function' && options.done($val);
+                options.done && typeof options.done === "function" && options.done($val);
                 options.elem.val('');
 
-                let content = "";
+                let content = '';
                 $.each(options.content, function (index, val) {
                     content += val + ","
                 });
@@ -126,13 +138,15 @@ layui.define(['jquery', 'layer'], function (exports) {
         })
     };
 
-    // 事件处理
+    /**
+     * 事件处理
+     */
     Class.prototype.events = function () {
         let that = this;
         let options = that.config;
-        $(options.elem.parent('.tags')).on('click', '.close', function () {
+        $(options.elem.parent(".tags")).on("click", '.close', function () {
             let ThisRemove = $(this).parent('span').remove();
-            let ThisText   = $(ThisRemove).find('em').text();
+            let ThisText   = $(ThisRemove).find("em").text();
             options.content.splice($.inArray(ThisText, options.content), 1);
             if (options.openFlatContent) {
                 options.flatContent.splice($.inArray(ThisText, options.flatContent), 1)
@@ -140,14 +154,18 @@ layui.define(['jquery', 'layer'], function (exports) {
         })
     };
 
-    // 核心入口
+    /**
+     * 核心入口
+     */
     inputTags.render = function (options) {
         let inst = new Class(options);
         inst.init();
         return thisInputTags.call(inst);
     };
 
-    // 加载样式
+    /**
+     * 加载样式
+     */
     layui.link(layui.cache.base + 'inputTags/inputTags.css');
 
     exports('inputTags', inputTags);
