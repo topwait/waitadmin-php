d1e0b5d81b06ce81eaa404061d9a284f9881bff5
zero <10652944+wafts@user.noreply.gitee.com>
2023-06-01 18:40:37 +0800

    优化api接口调用方式

---
+++ uniapp/src/api/designApi.js
@@ -0,0 +1,22 @@
+export default class {
+    /**
+     * 首页装修
+     */
+    static diyIndex() {
+        return uni.$u.http.get('diy/index')
+    }
+
+    /**
+     * 客服装修
+     */
+    static diyTie() {
+        return uni.$u.http.get('diy/tie')
+    }
+
+    /**
+     * 我的装修
+     */
+    static diyMe() {
+        return uni.$u.http.get('diy/me')
+    }
+}
