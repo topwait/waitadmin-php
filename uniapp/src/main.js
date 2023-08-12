9c1c875a583f66d94129739de768664d5cfde4c2
zero <10652944+wafts@user.noreply.gitee.com>
2023-08-12 17:39:29 +0800

    处理语言包

--- uniapp/src/main.js
+++ uniapp/src/main.js
@@ -1,15 +1,29 @@
 import { createSSRApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import route from '@/utils/request/route'
 import uHttp from '@/utils/request/http'
 import uView from '@/uni_modules/vk-uview-ui'
 import minxinsApp from '@/mixins/appMixin'
 import './styles/index.scss'
+import en from './locale/en.json'
+import zhHans from './locale/zh-Hans.json'
+import zhHant from './locale/zh-Hant.json'
+
+let i18nConfig = {
+    locale: uni.getLocale(),
+    messages:{
+        'en': en,
+        'zh-Hans': zhHans,
+        'zh-Hant': zhHant
+  	}
+}
 
 export function createApp() {
     const pinia = createPinia()
     const app = createSSRApp(App)
+    const i18n = createI18n(i18nConfig)
 
     app.config.globalProperties.$onLaunched = new Promise(resolve => {
         app.config.globalProperties.$isResolve = resolve
@@ -20,6 +34,7 @@ export function createApp() {
     app.use(pinia)
     app.use(uView)
     app.use(uHttp)
+    app.use(i18n)
     app.mixin(minxinsApp)
     route.intercept()
 
