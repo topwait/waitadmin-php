6984379ecefcba0e638d5f14fa4f2b35fc2b0228
zero <10652944+wafts@user.noreply.gitee.com>
2023-09-02 16:55:45 +0800

    调整版本号

--- server/config/project.php
+++ server/config/project.php
@@ -8,7 +8,7 @@ return [
     'backend_entrance' => env('project.backend', '/admin.php'),
 
     // 版本号
-    'version'       => '1.2.4',
+    'version'       => '1.2.5',
 
     // 上传器
     'uploader' => [
