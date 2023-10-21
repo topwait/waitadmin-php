34a9c9718bb814ee94495506e5622ab396c02f13
zero <10652944+wafts@user.noreply.gitee.com>
2023-10-21 15:22:23 +0800

    增加中间件

---
+++ server/app/common/http/middleware/DemoMiddleware.php
@@ -0,0 +1,64 @@
+<?php
+// +----------------------------------------------------------------------
+// | WaitAdmin快速开发后台管理系统
+// +----------------------------------------------------------------------
+// | 欢迎阅读学习程序代码,建议反馈是我们前进的动力
+// | 程序完全开源可支持商用,允许去除界面版权信息
+// | gitee:   https://gitee.com/wafts/WaitAdmin
+// | github:  https://github.com/topwait/waitadmin
+// | 官方网站: https://www.waitadmin.cn
+// | WaitAdmin团队版权所有并拥有最终解释权
+// +----------------------------------------------------------------------
+// | Author: WaitAdmin Team <2474369941@qq.com>
+// +----------------------------------------------------------------------
+
+namespace app\common\http\middleware;
+
+use app\common\exception\OperateException;
+use Closure;
+
+/**
+ * 演示站点中间件
+ */
+class DemoMiddleware
+{
+    /**
+     * 允许POST请求的接口
+     * @var array
+     */
+    protected array $needPostApi = [
+        'login/check',
+        'login/logout'
+    ];
+
+    /**
+     * 拦截处理
+     *
+     * @param $request
+     * @param Closure $next
+     * @return mixed
+     * @throws OperateException
+     * @author zero
+     */
+    public function handle($request, Closure $next): mixed
+    {
+        $response = $next($request);
+
+        if ($request->method() != 'POST' || !env('project.demo')) {
+            return $response;
+        }
+
+        $adminUser = session('adminUser')??[];
+        $adminId = intval($adminUser['id']??0);
+        if ($adminId === 1) {
+            return $response;
+        }
+
+        $accessUri = strtolower($request->controller() . '/' . $request->action());
+        if (!in_array($accessUri, $this->needPostApi)) {
+            throw new OperateException('演示环境不支持修改数据, 请下载源码本地部署体验!');
+        }
+
+        return $response;
+    }
+}
