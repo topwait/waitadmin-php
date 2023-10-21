d8c15ab593930ea750c95dcd2124d552199f0446
zero <10652944+wafts@user.noreply.gitee.com>
2023-10-21 15:11:02 +0800

    优化Api接口登录令牌自动续签

--- server/app/api/cache/LoginCache.php
+++ server/app/api/cache/LoginCache.php
@@ -1,15 +1,15 @@
 <?php
 // +----------------------------------------------------------------------
-// | WaitAdmin快速开发后台管理系统
+// | WaitChat智能聊天系统
 // +----------------------------------------------------------------------
-// | 欢迎阅读学习程序代码,建议反馈是我们前进的动力
-// | 程序完全开源可支持商用,允许去除界面版权信息
-// | gitee:   https://gitee.com/wafts/WaitAdmin
-// | github:  https://github.com/topwait/waitadmin
+// | 这不是一个自由软件,您只能在不用于商业目的的前提下对程序代码进行修改和使用。
+// | 任何企业和个人不允许对程序代码以任何形式任何目的再发布,商业使用请获取授权。
+// | 获取商业授权后,允许对程序进行二次开发修改,并且可进行商业运营使用。
+// +----------------------------------------------------------------------
 // | 官方网站: https://www.waitadmin.cn
 // | WaitAdmin团队版权所有并拥有最终解释权
 // +----------------------------------------------------------------------
-// | Author: WaitAdmin Team <2474369941@qq.com>
+// | Author: WaitAdmin Team <2474369941@qq.com | 2273716447@qq.com>
 // +----------------------------------------------------------------------
 declare (strict_types = 1);
 
@@ -36,7 +36,22 @@ class LoginCache
     public static function get(int $terminal, string $token): int
     {
         $cacheKey = self::$prefix.$terminal.':'.$token;
-        return intval(Cache::get($cacheKey, 0));
+        $cacheVal = Cache::get($cacheKey, '');
+        if (!$cacheVal) {
+            return 0;
+        }
+
+        $value = explode(':', $cacheVal);
+        $userId = intval($value[0]??0);
+        $time   = intval($value[1]??0);
+
+        // 续签令牌 (低于30分钟)
+        $expire = ($time + self::$ttl) - (60 * 30);
+        if ($time && time() >= $expire) {
+            self::set($userId, $terminal, $token);
+        }
+
+        return $userId;
     }
 
     /**
@@ -50,6 +65,20 @@ class LoginCache
     public static function set(int $userId, int $terminal, string $token): void
     {
         $cacheKey = self::$prefix.$terminal.':'.$token;
-        Cache::set($cacheKey, $userId, self::$ttl);
+        $cacheVal = $userId . ':' . time();
+        Cache::set($cacheKey, $cacheVal, self::$ttl);
+    }
+
+    /**
+     * 删除
+     *
+     * @param int $terminal
+     * @param string $token
+     * @author zero
+     */
+    public static function del(int $terminal, string $token)
+    {
+        $cacheKey = self::$prefix.$terminal.':'.$token;
+        Cache::delete($cacheKey);
     }
 }
