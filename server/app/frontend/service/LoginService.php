d833eafd8aa888ac94ed2bcb7b5e8100f50a098d
zero <10652944+wafts@user.noreply.gitee.com>
2023-12-19 14:24:11 +0800

    登录测试

--- server/app/frontend/service/LoginService.php
+++ server/app/frontend/service/LoginService.php
@@ -236,16 +236,18 @@ class LoginService extends Service
         // 微信授权
         $response = WeChatService::oaAuth2session($code);
         $response['terminal'] = ClientEnum::PC;
-        Log::write("\n\n===== 微信哈哈哈 =====\n");
-        Log::write(json_encode($response, JSON_UNESCAPED_UNICODE));
-        Log::write("\n\n");
 
         // 验证账户
         try {
             $userInfo = UserWidget::getUserAuthByResponse($response);
+
+            Log::write("\n\n===== 微信哈哈哈 =====\n");
+            Log::write(json_encode($userInfo, JSON_UNESCAPED_UNICODE));
+            Log::write("\n\n");
             if (empty($userInfo)) {
                 $userId = UserWidget::createUser($response);
             } else {
+                $response['user_id'] = $userInfo['user_id'];
                 $userId = UserWidget::updateUser($response);
             }
 
