2c055ede5f5f80200a3d711bf25ab98c7104333b
zero <10652944+wafts@user.noreply.gitee.com>
2024-09-21 16:01:17 +0800

    优化注释

--- server/app/api/controller/ArticleController.php
+++ server/app/api/controller/ArticleController.php
@@ -4,8 +4,8 @@
 // +----------------------------------------------------------------------
 // | 欢迎阅读学习程序代码,建议反馈是我们前进的动力
 // | 程序完全开源可支持商用,允许去除界面版权信息
-// | gitee:   https://gitee.com/wafts/WaitAdmin
-// | github:  https://github.com/topwait/waitadmin
+// | gitee:   https://gitee.com/wafts/waitadmin-php
+// | github:  https://gitee.com/wafts/waitadmin-php
 // | 官方网站: https://www.waitadmin.cn
 // | WaitAdmin团队版权所有并拥有最终解释权
 // +----------------------------------------------------------------------
