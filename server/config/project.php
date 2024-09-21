68fe8b4084b72dd6d797dc99381434f1bbe9e0ce
zero <10652944+wafts@user.noreply.gitee.com>
2024-09-21 21:00:48 +0800

    优化代码

--- server/config/project.php
+++ server/config/project.php
@@ -8,7 +8,7 @@ return [
     'backend_entrance' => env('project.backend', '/admin.php'),
 
     // 版本号
-    'version'       => '1.3.3',
+    'version'       => '1.3.4',
 
     // 上传器
     'uploader' => [
