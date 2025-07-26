cf2f125d2c6453efb0b47a0b63b64d2fda802439
zero <10652944+wafts@user.noreply.gitee.com>
2025-07-26 23:06:53 +0800

    update: uniapp

--- uniapp/vite.config.js
+++ uniapp/vite.config.js
@@ -4,5 +4,13 @@ import uni from '@dcloudio/vite-plugin-uni'
 export default defineConfig({
     plugins: [
         uni(),
-    ]
+    ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler',
+                silenceDeprecations: ['legacy-js-api']
+            }
+        }
+    }
 })
