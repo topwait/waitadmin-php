3b997fb745de1cc118cba19078ad18e40ef88dba
zero <10652944+wafts@user.noreply.gitee.com>
2025-08-18 21:35:24 +0800

    update: 优化代码

--- server/config/project.php
+++ server/config/project.php
@@ -12,9 +12,21 @@ return [
 
     // 上传器
     'uploader' => [
-        'image'    => ['size'=>(10 * 1024 * 1024), 'ext'=>['png','jpg','jpeg','gif','ico','bmp']],
-        'video'    => ['size'=>(30 * 1024 * 1024), 'ext'=>['mp4','mp3','avi','flv','rmvb','mov']],
-        'package'  => ['size'=>(30 * 1024 * 1024), 'ext'=>['zip','rar','iso','7z','tar','gz','arj','bz2']],
-        'document' => ['size'=>(30 * 1024 * 1024), 'ext'=>['txt','doc','docx','xls','xlsx','ppt','pptx','pdf','pem']]
+        'image'    => [
+            'size'=>(10 * 1024 * 1024),
+            'ext'=>['png','jpg','jpeg','gif','ico','bmp']
+        ],
+        'video'    => [
+            'size'=>(30 * 1024 * 1024),
+            'ext'=>['mp4','mp3','avi','flv','rmvb','mov']
+        ],
+        'package'  => [
+            'size'=>(30 * 1024 * 1024),
+            'ext'=>['zip','rar','iso','7z','tar','gz','arj','bz2']
+        ],
+        'document' => [
+            'size'=>(30 * 1024 * 1024),
+            'ext'=>['txt','doc','docx','xls','xlsx','ppt','pptx','pdf','pem']
+        ]
     ]
 ];
