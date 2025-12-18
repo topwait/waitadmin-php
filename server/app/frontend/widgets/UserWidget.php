0ee42a3050dc1d0869edc2fa62d7d82f6ac9cbd0
zero <10652944+wafts@user.noreply.gitee.com>
2025-12-18 15:43:37 +0800

    强制绑定手机处理

--- server/app/frontend/widgets/UserWidget.php
+++ server/app/frontend/widgets/UserWidget.php
@@ -53,6 +53,14 @@ class UserWidget extends Service
         $mobile   = $response['mobile']    ?? '';
         $gender   = intval($response['gender'] ?? 0);
 
+        $k = match ($terminal) {
+            ClientEnum::PC => 'pc',
+            ClientEnum::H5 => 'h5',
+            ClientEnum::MNP,
+            ClientEnum::OA => 'wx',
+            default => 'other'
+        };
+
         // 密码信息
         $salt = make_rand_char(7-1);
         if (!empty($password)) {
@@ -60,7 +68,7 @@ class UserWidget extends Service
         }
 
         // 强制绑定
-        $forceMobile = ConfigUtils::get('login', 'force_mobile', 0);
+        $forceMobile = boolval(ConfigUtils::get('login', $k, 'force_mobile')??false);
         if ($forceMobile && !$mobile) {
             $data = ['sign'=>md5(time().make_rand_char(9))];
             EnrollCache::set($data['sign'], $response);
