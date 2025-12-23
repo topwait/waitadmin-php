7b1ddfc89f4b936a0e9747be04ea18e161433df8
zero <10652944+wafts@user.noreply.gitee.com>
2025-12-23 21:16:32 +0800

    移除旧版本 (uniapp)端 - 即uview开发的

--- uniapp/src/enums/cacheEnum.js
+++
@@ -1,4 +0,0 @@
-export default {
-    TOKEN_KEY: 'token',  // 登录令牌
-    FOOTPRINT: 'pages'   // 页面足迹
-}
--- uniapp/src/enums/clientEnum.js
+++
@@ -1,7 +0,0 @@
-export const ClientEnum = {
-    MP_WEIXIN: 1, // 微信小程序
-    OA_WEIXIN: 2, // 微信公众号
-    H5: 3,        // H5
-    IOS: 5,       // 苹果
-    ANDROID: 6    // 安卓
-}
